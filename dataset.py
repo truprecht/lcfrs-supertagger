799fee61927b3f8ad684b19e08f85fbd1d05363a
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2020-08-12 15:51:03 +0200

    init evaluation

--- dataset.py
+++ dataset.py
@@ -33,26 +33,33 @@ class SupertagDataset(Dataset):
     def __init__(self, c, stoi, tag_distance=1):
         # TODO: sort by length
         self.dims = len(c.pos), len(c.preterms), len(c.supertags)
-        self.sentences = tuple( tensor([stoi(w) for w in sentence]) for sentence in c.sent_corpus )
+        self.sentence_embeddings = tuple( tensor([stoi(w) for w in sentence]) for sentence in c.sent_corpus )
+        self.sentences = c.sent_corpus
         self.pos_tags = tuple( tensor(pos) for pos in c.pos_corpus )
         self.preterminals = tuple( tensor(prets) for prets in c.preterm_corpus )
         self.supertags = tuple( tensor(st) for st in c.supertag_corpus )
         self.supertag_distances = tensor(-tag_distance * c.confusion_matrix).softmax(dim=1).double()
+        self.trees = c.tree_corpus
 
     def __getitem__(self, key):
-        return self.sentences[key], self.pos_tags[key], \
-            self.preterminals[key], self.supertags[key]
+        return self.sentences[key], self.sentence_embeddings[key], self.trees[key], \
+            self.pos_tags[key], self.preterminals[key], self.supertags[key]
 
     def __len__(self):
         return len(self.sentences)
 
     def collate_training(self, results):
-        (words, pos, preterms, tags) = zip(*results)
-        lens = tuple(len(sentence) for sentence in words)
+        (_, word_embeddings, _, pos, preterms, tags) = zip(*results)
+        lens = tuple(len(sentence) for sentence in word_embeddings)
         tags = tuple(self.supertag_distances[tag] for tag in tags)
-        return tuple(pad_sequence(batch, padding_value=-1) for batch in (words, pos, preterms, tags)) + (tensor(lens),)
+        return tuple(pad_sequence(batch, padding_value=-1) for batch in (word_embeddings, pos, preterms, tags)) + (tensor(lens),)
 
     def collate_test(self, results):
-        (words, pos, preterms, tags) = zip(*results)
+        (_, word_embeddings, _, pos, preterms, tags) = zip(*results)
+        lens = tuple(len(sentence) for sentence in word_embeddings)
+        return tuple(pad_sequence(batch, padding_value=-1) for batch in (word_embeddings, pos, preterms, tags)) + (tensor(lens),)
+
+    def collate_val(self, results):
+        (words, word_embeddings, trees, pos, preterms, tags) = zip(*results)
         lens = tuple(len(sentence) for sentence in words)
-        return tuple(pad_sequence(batch, padding_value=-1) for batch in (words, pos, preterms, tags)) + (tensor(lens),)
+        return (words, trees) + tuple(pad_sequence(batch, padding_value=-1) for batch in (word_embeddings, pos, preterms, tags)) + (tensor(lens),)
