e28224feef1400f2b2c297766a5037bc29937226
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2020-09-03 11:18:37 +0200

    reflect latest changes to grid search

--- grid_search.py
+++ grid_search.py
@@ -1,13 +1,14 @@
 from os.path import abspath, expanduser
 from torch import device, cuda
 
-from ray import tune
+from ray import tune, init
 from functools import partial
-from training import load_data, train_model, read_config, tagger
+from training import *
 
 config = read_config()
 # use absolute path, b/c ray can't handle relative ones
-config["Data"]["corpus"] = abspath(expanduser(config["Data"]["corpus"]))
+config["Data"]["corpusfilename"] = abspath(expanduser(config["Data"]["corpusfilename"]))
+config["Test"]["evalfilename"] = abspath(expanduser(config["Test"]["evalfilename"]))
 
 def tune_report(scores, epoch_or_iteration):
     tune.report(**scores)
@@ -16,13 +17,13 @@ torch_device = device("cpu")
 ray_devices = { "cpu": 1, "gpu": 1 } if torch_device.type == "cuda" else { "cpu": 1 }
 print(f"running on device {torch_device}")
 
-def setup_and_train_model(training_conf, data_conf=None):
-    (train, test, _), data = load_data(data_conf["Data"], \
-        tag_distance=int(training_conf["tag_distance"]))
+def setup_and_train_model(data_conf, test_conf, training_conf):
+    (train, test, _), data = load_data(loadconfig(**data_conf))
 
-    model = tagger(data.dims, tagger.Hyperparameters.from_dict(training_conf)).double()
+    tc = trainparam(**training_conf, **test_conf)
+    model = tagger(data.dims, tc).double()
     model.to(torch_device)
-    train_model(model, training_conf, train, test, \
+    train_model(model, tc, train, test, data, \
         torch_device=torch_device, report_loss=tune_report)
 
 grid_axes = set(config["Gridsearch"]["grid_axes"].split())
@@ -38,12 +39,13 @@ for key in config["Gridsearch"]:
         tune_parameters[key] = config["Gridsearch"][key]
 
 analysis = tune.run(
-    partial(setup_and_train_model, data_conf=config),
+    partial(setup_and_train_model, config["Data"], config["Test"]),
     name="Supertag-gridsearch",
     config=tune_parameters,
     num_samples=grid_samples,
     resources_per_trial=ray_devices,
     progress_reporter=tune.CLIReporter(max_progress_rows=5, max_error_rows=5, max_report_frequency=1800),
-    verbose=1
+    verbose=1,
+    fail_fast=True
 )
-print(analysis.get_best_config(metric="loss/test/combined"))
+print(analysis.get_best_config(metric="lf/test/parse"))
