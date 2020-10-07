7fd9a04f6d7fc0da8e9fc6d5129aa71bf006027a
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2020-10-07 15:51:23 +0200

    only support sequential splits w/ absolute portions

--- dump_conll.py
+++ dump_conll.py
@@ -7,45 +7,29 @@ config = loadconfig(**read_config()["Data"])
 corpus = SupertagCorpus.read(open(config.corpusfilename, "rb"))
 
 size = len(corpus.sent_corpus)
-splittype, *portions = config.split.split()
+portions = config.split.split()
 names = "train dev test".split()
 
-assert splittype in ["hn", "vc", "dk"], (
-    "split must be of the form `(hn|vc|dk) <train> <dev> <test>`, where"
-    "hn rotates `test`, `dev` and `train` sentences for the test, developement and training set"
-    "vc assumes ratios for `train`, `dev`, `test` and splits the sentences sequentially"
-    "dk assumes absolute values and splits the sentences sequentially")
-
-if splittype == "vc":
-    portions = tuple(round(float(portion) * size) for portion in portions)
-else:
-    portions = tuple(int(portion) for portion in portions)
-
-if splittype == "hn":
-    mod = sum(portions)
-    names = names[::-1]
-    portions = portions[::-1]
-else:
-    mod = size
+portions = tuple(int(portion) for portion in portions)
+assert len(portions) in [3,4]
 
 limits = tuple(reduce(lambda xs, y: xs + (xs[-1]+y,), portions, (0,)))
-limits = tuple(zip(names, limits[:-1], limits[1:]))
+limits = tuple(zip(names, limits[-4:-1], limits[-3:]))
 
-files = { (name, tt): open(f"{config.corpusfilename}.{name}.{tt}", "w") for name in names for tt in ("trees", "tags") }
+print(limits, size)
 
 tdtidx = 0
-for sentidx in range(size):
-    while (sentidx%mod) < limits[tdtidx][1] or (sentidx%mod) >= limits[tdtidx][2]:
-        tdtidx = (tdtidx+1) % len(limits)
-    tagfile = files[(limits[tdtidx][0], "tags")]
-    treefile = files[(limits[tdtidx][0], "trees")]
-    for (word, pos, preterm, supertag) in zip(
-            corpus.sent_corpus[sentidx],
-            corpus.pos_corpus[sentidx],
-            corpus.preterm_corpus[sentidx],
-            corpus.supertag_corpus[sentidx]):
-        print(f"{word} {corpus.pos[pos]} {corpus.preterms[preterm]} {corpus.supertags[supertag].pos()}", file=tagfile)
-    print(file=tagfile)
-    print(corpus.tree_corpus[sentidx], file=treefile)
-
-for file in files.values(): file.close()
+for (name, start, stop) in limits:
+    tagfile = open(f"{config.corpusfilename}.{name}.tags", "w")
+    treefile = open(f"{config.corpusfilename}.{name}.trees", "w")
+    for sentidx in range(start, stop):
+        for (word, pos, preterm, supertag) in zip(
+                corpus.sent_corpus[sentidx],
+                corpus.pos_corpus[sentidx],
+                corpus.preterm_corpus[sentidx],
+                corpus.supertag_corpus[sentidx]):
+            print(f"{word} {corpus.pos[pos]} {corpus.preterms[preterm]} {corpus.supertags[supertag].pos()}", file=tagfile)
+        print(file=tagfile)
+        print(corpus.tree_corpus[sentidx], file=treefile)
+    treefile.close()
+    tagfile.close()
