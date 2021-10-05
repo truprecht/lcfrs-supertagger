d75888b5d28cd6a0cb755231ea1b9c09da368c7b
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2021-10-05 12:52:39 +0200

    fix evalutation and fallback initialization

--- evaluate.py
+++ evaluate.py
@@ -5,7 +5,8 @@ import flair
 import torch
 
 from supertagging.data import SupertagCorpusFile, corpusparam
-from supertagging.model import Supertagger, EvalParameters
+from supertagging.model import EvalParameters
+from supertagging.tagging.tagger_model import DecoderModel
 
 import logging
 logging.getLogger("flair").setLevel(40)
@@ -16,6 +17,7 @@ args.add_argument("data", help="configuration file containing at least the [Corp
 args.add_argument("model", help="trained model file as it is saved by training.py")
 args.add_argument("-o", nargs="+", help="override options in the [Eval] section of the configuration", metavar="option=value")
 args.add_argument("--device", type=torch.device, help="sets the torch device", choices=["cpu"]+[f"cuda:{n}" for n in range(torch.cuda.device_count())])
+args.add_argument("--dev", action="store_true", help="Evaluate on dev set instead of test set")
 args = args.parse_args()
 
 if args.device:
@@ -32,10 +34,11 @@ ec = EvalParameters(**ecdict)
 
 lc = corpusparam(**config["Corpus"], **config["Grammar"])
 with SupertagCorpusFile(lc) as cf:
-    model = Supertagger.load(args.model)
+    model = DecoderModel.load(args.model)
     model.set_eval_param(ec)
     model.eval()
-    results, _ = model.evaluate(cf.corpus.test, mini_batch_size=ec.batchsize,
+    data = cf.corpus.dev if args.dev else cf.corpus.test
+    results, _ = model.evaluate(data, mini_batch_size=ec.batchsize,
         only_disc=ec.only_disc, accuracy=ec.accuracy, return_loss=False, othertag_accuracy=ec.othertag_accuracy)
     print(results.log_header)
     print(results.log_line)
